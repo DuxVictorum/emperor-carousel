b70564d6c9fba456f95e682e4ecc5a175d3c6025
Mark Thorne <thorne.mark@gmail.com>
2022-02-11 23:19:07 -0600

    Added info for Tiberius and image file/URL data for coin images

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const rightButton = document.querySelector(".right-button");
 const coinObverse = document.querySelector(".coin-obverse");
 const coinReverse = document.querySelector(".coin-reverse");
 
-leftButton.addEventListener("click", e => {
-  coinObverse.src = "assets/aureus_Nero21.jpg";
-  coinReverse.src = "assets/aureus_rev_Nero21.jpg";
+rightButton.addEventListener("click", e => {
+  coinObverse.src = "assets/02-Tiberius27.jpg";
+  coinReverse.src = "assets/02-Tiberius27_rev.jpg";
 });
