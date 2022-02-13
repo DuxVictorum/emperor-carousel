91929c2279f12c67f35d452e3b8f7e68fdab1a04
Mark Thorne <thorne.mark@gmail.com>
2022-02-13 15:49:01 -0600

    Set up initial data box for emperor information

--- script.js
+++ script.js
@@ -5,16 +5,23 @@ const rightButton = document.querySelector(".right-button");
 const coinObverse = document.querySelector(".coin-obverse");
 const coinReverse = document.querySelector(".coin-reverse");
 
+const name = document.querySelector(".name");
+const fullName = document.querySelector(".fullName");
+
 let currentEmperor = 0;
 
 rightButton.addEventListener("click", e => {
   currentEmperor += 1;
   coinObverse.src = coinData[currentEmperor].coinFile[0];
   coinReverse.src = coinData[currentEmperor].coinFile[1];
+  name.innerText = coinData[currentEmperor].emperorName;
+  fullName.innerText = coinData[currentEmperor].fullName;
 });
 
 leftButton.addEventListener("click", e => {
   currentEmperor -= 1;
   coinObverse.src = coinData[currentEmperor].coinFile[0];
   coinReverse.src = coinData[currentEmperor].coinFile[1];
+  name.innerText = coinData[currentEmperor].emperorName;
+  fullName.innerText = coinData[currentEmperor].fullName;
 });
