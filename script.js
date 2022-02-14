4700e421c81c836047c70388225f25b72d836e00
Mark Thorne <thorne.mark@gmail.com>
2022-02-13 22:42:13 -0600

    Filled out more of the emperor info section; added function for adding in notableFor array as list elements

--- script.js
+++ script.js
@@ -8,15 +8,18 @@ const coinReverse = document.querySelector(".coin-reverse");
 const name = document.querySelector(".name");
 const fullName = document.querySelector(".fullName");
 
+const notableFor = document.querySelector(".notableFor");
+
 let currentEmperor = 0;
 
-rightButton.addEventListener("click", e => {
-  currentEmperor += 1;
-  coinObverse.src = coinData[currentEmperor].coinFile[0];
-  coinReverse.src = coinData[currentEmperor].coinFile[1];
-  name.innerText = coinData[currentEmperor].emperorName;
-  fullName.innerText = coinData[currentEmperor].fullName;
-});
+function notableBits(stuff) {
+  let notableStuff = "";
+  for (let i = 0; i < stuff.length; i++) {
+    notableStuff += "<li>" + stuff[i] + "</li>";
+  }
+  console.log(notableStuff);
+  return notableStuff;
+}
 
 leftButton.addEventListener("click", e => {
   currentEmperor -= 1;
@@ -24,4 +27,16 @@ leftButton.addEventListener("click", e => {
   coinReverse.src = coinData[currentEmperor].coinFile[1];
   name.innerText = coinData[currentEmperor].emperorName;
   fullName.innerText = coinData[currentEmperor].fullName;
+
+  notableFor.innerHTML = notableBits(coinData[currentEmperor].notableFor);
+});
+
+rightButton.addEventListener("click", e => {
+  currentEmperor += 1;
+  coinObverse.src = coinData[currentEmperor].coinFile[0];
+  coinReverse.src = coinData[currentEmperor].coinFile[1];
+  name.innerText = coinData[currentEmperor].emperorName;
+  fullName.innerText = coinData[currentEmperor].fullName;
+
+  notableFor.innerHTML = notableBits(coinData[currentEmperor].notableFor);
 });
